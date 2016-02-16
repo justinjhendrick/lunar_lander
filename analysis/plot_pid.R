7f98a525eaa732814d1bffb4d6053d2fff0ecfea
Justin Hendrick <justinjhendrick@gmail.com>
2016-02-16 12:55:25 -0500

    cleanup pid plot

--- analysis/plot_pid.R
+++ analysis/plot_pid.R
@@ -2,9 +2,16 @@ data = read.csv("pid.csv")
 
 pdf(file="tune_pid.pdf")
 matplot(data,
-        main="Wrapped PID tuning for lunar_lander",
-        ylab="value",
-        xlab="frame number")
-legend("bottomleft", c('desired (r)', 'current (r)', 'manip_var (r/s^2)'), col=1:4, pch=1)
+        main="PID tuning for point_retrograde()",
+        ylab="value (radians or radians/second^2)",
+        xlab="time (milliseconds)",
+        pch=1)
+abline(h=0, lty=2)
+legend("bottom",
+       c('desired orientation',
+         'current orientation',
+         'manipulated (r/s^2)'),
+       col=1:3,
+       pch=1)
 
 dev.off()
